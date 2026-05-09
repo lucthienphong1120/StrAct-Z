8c93cdec5e2318a15876a7c227d94fa0a7630c39
Thien Phong <aichoibangbangko@gmail.com>
2026-05-09 11:56:49 +0700

    fix bug

--- public/js/app.js
+++ public/js/app.js
@@ -55,7 +55,8 @@ async function checkAuth() {
       document.getElementById('dashboard').style.display = 'block';
       document.getElementById('connectPrompt').style.display = 'none';
       document.getElementById('stravaSection').style.display = 'block';
-      document.getElementById('historySection').style.display = 'block';
+      document.getElementById('configSection').style.display = 'flex';
+      document.getElementById('historySection').style.display = 'flex';
       document.getElementById('stravaAccountCard').style.display = 'block';
       document.getElementById('btnLogout').style.display = 'block';
       renderAccountInfo(data.athlete);
@@ -66,6 +67,7 @@ async function checkAuth() {
       document.getElementById('dashboard').style.display = 'block';
       document.getElementById('connectPrompt').style.display = 'block';
       document.getElementById('stravaSection').style.display = 'none';
+      document.getElementById('configSection').style.display = 'none';
       document.getElementById('historySection').style.display = 'none';
       document.getElementById('stravaAccountCard').style.display = 'none';
       document.getElementById('btnLogout').style.display = 'block';
