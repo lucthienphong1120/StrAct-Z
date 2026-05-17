f583ff19e2042cb3d873a87ddbf728b256af469c
Thien Phong <aichoibangbangko@gmail.com>
2026-05-17 15:34:44 +0700

    Remove temp script

--- fix-tooltips.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('public/index.html', 'utf8');
-data = data.replace(/data-tooltip="([^"]*)"/g, 'data-tooltip="?"');
-fs.writeFileSync('public/index.html', data);
-console.log('done');
